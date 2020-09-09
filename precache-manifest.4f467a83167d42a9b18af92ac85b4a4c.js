a5820d731b76ece44dbd320e6b0241bd128ea895
tulikumari <info.tulikumari@gmail.com>
2020-09-09 18:48:56 +0530

    Updates

---
+++ precache-manifest.4f467a83167d42a9b18af92ac85b4a4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "344d9cf8b3fee6a43572b5daea20e565",
+    "url": "/inventory/index.html"
+  },
+  {
+    "revision": "8445325e0cee6554f432",
+    "url": "/inventory/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a4ec480e2b905fbce400",
+    "url": "/inventory/static/css/main.05721837.chunk.css"
+  },
+  {
+    "revision": "8445325e0cee6554f432",
+    "url": "/inventory/static/js/2.b79f2d73.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/inventory/static/js/2.b79f2d73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4ec480e2b905fbce400",
+    "url": "/inventory/static/js/main.93c3a409.chunk.js"
+  },
+  {
+    "revision": "771edf661ca769247f3c",
+    "url": "/inventory/static/js/runtime-main.128bf115.js"
+  }
+]);
